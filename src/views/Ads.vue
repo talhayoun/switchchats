8d7eb1b455e7eba4e61bb275e9f27f26d270c348
Liel <liel@contra-adv.co.il>
2023-01-17 13:47:14 +0200

    shalev test

--- src/views/Ads.vue
+++ src/views/Ads.vue
@@ -7,6 +7,7 @@
       <div class="OUTBRAIN" data-src="DROP_PERMALINK_HERE" data-widget-id="AR_3"></div>
       <script type="text/javascript" async="async" src="https://widgets.outbrain.com/outbrain.js"></script>
       <p>בדיקה בפנים</p>
+      <div id="outbrain_widget"></div>
     </div>
     <div class="OUTBRAIN" data-src="DROP_PERMALINK_HERE" data-widget-id="MB_20"></div>
     <script type="text/javascript" async="async" src="https://widgets.outbrain.com/outbrain.js"></script>
@@ -18,70 +19,86 @@
     </div>
   </div>
 </template>
-
 <script>
-export default {
-  methods: {
-    handleLeave: function () {
-      this.$router.push('/')
+  import 'https://widgets.outbrain.com/outbrain.js'
+
+  export default {
+    name: 'Ads',
+    mounted() {
+      OBR.extern('outbrain_widget', {
+        widgetId: 'AR_3',
+        type: 'outbrain',
+        options: {
+          section: 'Sports'
+        },
+
+      });
     }
   }
-}
+</script>
+<script>
+  export default {
+    methods: {
+      handleLeave: function () {
+        this.$router.push('/')
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-.ads {
-  max-width: 640px;
+  .ads {
+    max-width: 640px;
 
-  .ads-text {
-    padding: 8px 12px;
+    .ads-text {
+      padding: 8px 12px;
 
-    p {
-      margin: 0;
-      font-size: 1rem;
-      font-weight: 500;
-      text-align: center;
-      color: #6D8FBA;
+      p {
+        margin: 0;
+        font-size: 1rem;
+        font-weight: 500;
+        text-align: center;
+        color: #6D8FBA;
+      }
     }
-  }
 
-  .ads-container {
-    display: flex;
-    flex-wrap: wrap;
+    .ads-container {
+      display: flex;
+      flex-wrap: wrap;
 
-    .ads-item {
-      flex: 0 1 calc(50% - 8px);
+      .ads-item {
+        flex: 0 1 calc(50% - 8px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 160px;
+        margin: 0 4px;
+        border: 2px solid #dbe3ee;
+        border-radius: 8px;
+        overflow: hidden;
+
+        h4 {
+          font-size: 1.4rem;
+          font-weight: 500;
+          color: #dbe3ee;
+        }
+
+        .ads-item-container {
+          height: 100%;
+        }
+      }
+
+      .ads-item:last-child {
+        flex: 0 1 calc(100% - 8px);
+        margin-top: 8px;
+        height: 80px;
+      }
+    }
+
+    .window-action {
       display: flex;
       justify-content: center;
-      align-items: center;
-      height: 160px;
-      margin: 0 4px;
-      border: 2px solid #dbe3ee;
-      border-radius: 8px;
-      overflow: hidden;
-
-      h4 {
-        font-size: 1.4rem;
-        font-weight: 500;
-        color: #dbe3ee;
-      }
-
-      .ads-item-container {
-        height: 100%;
-      }
-    }
-
-    .ads-item:last-child {
-      flex: 0 1 calc(100% - 8px);
-      margin-top: 8px;
-      height: 80px;
+      margin: 16px 0;
     }
   }
-
-  .window-action {
-    display: flex;
-    justify-content: center;
-    margin: 16px 0;
-  }
-}
 </style>
