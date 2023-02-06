b96e800c818e350521f4e29ea5a016151cbad8a7
שלו אלייה <shalev elia>
2023-02-06 19:42:20 +0200

    fix build

--- src/helpers/apiHelper.js
+++ src/helpers/apiHelper.js
@@ -1,28 +1,32 @@
-const axios = require('axios')
+const axios = require("axios");
 
 const apiHelper = {
   attachRoom(roomId) {
-    axios.get('https://api.skippi.net/room?roomId=' + roomId).then(room => {
-      console.log('room attached')
-    })
+    axios.get("https://api.skippi.net/room?roomId=" + roomId).then((room) => {
+      console.log("room attached");
+    });
   },
   leaveRoom(roomId) {
-    axios.get('https://api.skippi.net/leave-room?roomId=' + roomId).then(room => {
-      console.log('leave room')
-    })
+    axios
+      .get("https://api.skippi.net/leave-room?roomId=" + roomId)
+      .then((room) => {
+        console.log("leave room");
+      });
   },
   message(uid, msg, roomId) {
-    axios.get('https://api.skippi.net/message?uid=' + uid + '&m=' + encodeURI(msg) + '&roomId=' + roomId).then(msg_ => {
-      console.log('message sent')
-    })
+    axios
+      .get(
+        "https://api.skippi.net/message?uid=" +
+          uid +
+          "&m=" +
+          encodeURI(msg) +
+          "&roomId=" +
+          roomId
+      )
+      .then((msg_) => {
+        console.log("message sent");
+      });
   },
-  /*
-  report(roomId, reportType, reportText) {
-    axios.get('https://api.skippi.net/report?type=' + reportType + '&text=' + reportText + '&' + 'roomId=' + roomId).then(msg_ => {
-      console.log('user reported')
-    })
-  }
-  */
-}
+};
 
-export default apiHelper
+export default apiHelper;
