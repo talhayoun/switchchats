e7af75e10df0d0f8da638364309af44ded661c62
שלו אלייה <shalev elia>
2023-02-06 19:47:12 +0200

    fix build

--- src/helpers/apiHelper.js
+++ src/helpers/apiHelper.js
@@ -1,32 +1,21 @@
-const axios = require("axios");
+const axios = require('axios')
 
 const apiHelper = {
   attachRoom(roomId) {
-    axios.get("https://api.skippi.net/room?roomId=" + roomId).then((room) => {
-      console.log("room attached");
-    });
+    axios.get('https://api.skippi.net/room?roomId=' + roomId).then(room => {
+      console.log('room attached')
+    })
   },
   leaveRoom(roomId) {
-    axios
-      .get("https://api.skippi.net/leave-room?roomId=" + roomId)
-      .then((room) => {
-        console.log("leave room");
-      });
+    axios.get('https://api.skippi.net/leave-room?roomId=' + roomId).then(room => {
+      console.log('leave room')
+    })
   },
   message(uid, msg, roomId) {
-    axios
-      .get(
-        "https://api.skippi.net/message?uid=" +
-          uid +
-          "&m=" +
-          encodeURI(msg) +
-          "&roomId=" +
-          roomId
-      )
-      .then((msg_) => {
-        console.log("message sent");
-      });
-  },
-};
+    axios.get('https://api.skippi.net/message?uid=' + uid + '&m=' + encodeURI(msg) + '&roomId=' + roomId).then(msg_ => {
+      console.log('message sent')
+    })
+  }
+}
 
-export default apiHelper;
+export default apiHelper
